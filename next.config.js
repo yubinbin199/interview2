4e9e5ea2b19302782d8d54fd4cfcc353c6e3f4d4
ctw <ctw@MacBook-Air-3.local>
2025-05-22 15:54:47 +0800

    Build static site for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
   reactStrictMode: true,
   // 移除静态导出设置，Vercel原生支持Next.js
   typescript: {
@@ -9,7 +11,9 @@ const nextConfig = {
   eslint: {
     // 部署时忽略ESLint错误
     ignoreDuringBuilds: true,
-  }
+  },
+  basePath: '',
+  trailingSlash: true
 };
 
 module.exports = nextConfig; 
